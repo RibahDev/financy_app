cb36de3fdeee3b90d5d6a2b2b2d427b087ccccce
RibahDev <joao.ribeiro17@outlook.com>
2025-03-24 21:10:04 -0300

    Adicionando fonts e images ao projeto

---
+++ lib/common/constants/app_text_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  AppTextStyles._();
+
+  static const TextStyle bigText = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 50.0,
+    fontWeight: FontWeight.w700,
+    color: Colors.white,
+  );
+}
