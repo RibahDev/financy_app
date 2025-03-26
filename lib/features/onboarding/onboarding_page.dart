d5fc2bdae210b1cf4f3f9f989fbca2234c7f16e6
RibahDev <joao.ribeiro17@outlook.com>
2025-03-26 20:30:14 -0300

    Refactor no text com onPressed, passando para widget separado

--- lib/features/onboarding/onboarding_page.dart
+++ lib/features/onboarding/onboarding_page.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:financy_app/common/constants/app_colors.dart';
 import 'package:financy_app/common/constants/app_text_styles.dart';
+import 'package:financy_app/common/widgets/multi_text_button.dart';
 import 'package:financy_app/common/widgets/primary_button.dart';
 import 'package:flutter/material.dart';
 
@@ -29,43 +30,34 @@ class OnboardingPage extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 32.0,
-                vertical: 16.0,
+              padding: const EdgeInsets.only(
+                left: 32.0,
+                right: 32.0,
+                top: 16.0,
+                bottom: 4.0,
               ),
               child: PrimaryButton(text: 'Get Started', onPressed: () {}),
             ),
-            CustomTextButton(),
+            MultiTextButton(
+              onPressed: () => log('tap'),
+              children: [
+                Text(
+                  'Already have account?',
+                  style: AppTextStyles.smallText.copyWith(
+                    color: AppColors.grey,
+                  ),
+                ),
+                Text(
+                  ' Log In',
+                  style: AppTextStyles.smallText.copyWith(
+                    color: AppColors.greenlightOne,
+                  ),
+                ),
+              ],
+            ),
             const SizedBox(height: 45.0),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class CustomTextButton extends StatelessWidget {
-  const CustomTextButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () => log('tap'),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Already have account?',
-            style: AppTextStyles.smallText.copyWith(color: AppColors.grey),
-          ),
-          Text(
-            ' Log In',
-            style: AppTextStyles.smallText.copyWith(
-              color: AppColors.greenlightOne,
-            ),
-          ),
-        ],
       ),
     );
   }
