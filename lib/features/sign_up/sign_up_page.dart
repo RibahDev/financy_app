69df7f4cc67437a8e8e2f827e85fe72b05d5e76a
RibahDev <joao.ribeiro17@outlook.com>
2025-03-26 22:49:14 -0300

    Iniciando estrutura da página de cadastro

---
+++ lib/features/sign_up/sign_up_page.dart
@@ -0,0 +1,60 @@
+import 'dart:developer';
+
+import 'package:financy_app/common/constants/app_colors.dart';
+import 'package:financy_app/common/constants/app_text_styles.dart';
+import 'package:financy_app/common/widgets/multi_text_button.dart';
+import 'package:financy_app/common/widgets/primary_button.dart';
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Text(
+            'Spend Smarter',
+            style: AppTextStyles.mediumText.copyWith(
+              color: AppColors.greenlightTwo,
+            ),
+          ),
+          Text(
+            'Save More',
+            style: AppTextStyles.mediumText.copyWith(
+              color: AppColors.greenlightTwo,
+            ),
+          ),
+          Image.asset('assets/images/sign.png'),
+          TextFormField(),
+          TextField(),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 32.0,
+              right: 32.0,
+              top: 20.0,
+              bottom: 4.0,
+            ),
+            child: PrimaryButton(text: 'Sign Up', onPressed: () => log('btn')),
+          ),
+          MultiTextButton(
+            onPressed: () => log('tap'),
+            children: [
+              Text(
+                'Already have account?',
+                style: AppTextStyles.smallText.copyWith(color: AppColors.grey),
+              ),
+              Text(
+                ' Log In',
+                style: AppTextStyles.smallText.copyWith(
+                  color: AppColors.greenlightOne,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
