af55f8bd65a8f533f9d7ead352eb2331c0469172
RibahDev <joao.ribeiro17@outlook.com>
2025-04-09 22:36:58 -0300

    Criando locator utilizando a dependência get_it

--- lib/features/sign_up/sign_up_page.dart
+++ lib/features/sign_up/sign_up_page.dart
@@ -13,8 +13,8 @@ import 'package:financy_app/common/widgets/password_form_field.dart';
 import 'package:financy_app/common/widgets/primary_button.dart';
 import 'package:financy_app/features/sign_up/sign_up_controller.dart';
 import 'package:financy_app/features/sign_up/sign_up_state.dart';
+import 'package:financy_app/locator.dart';
 
-import 'package:financy_app/services/mock_auth_service.dart';
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatefulWidget {
@@ -29,7 +29,7 @@ class _SignUpPageState extends State<SignUpPage> {
   final _nameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
-  final _controller = SignUpController(MockAuthService());
+  final _controller = locator.get<SignUpController>();
 
   @override
   void dispose() {
